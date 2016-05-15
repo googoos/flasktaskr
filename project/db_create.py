97e351a082966a76f790dbd525c21343935bd61f
Gugus A. Sakti <gugussakti@gmail.com>
2016-05-15 07:31:07 +0700

    add config and database

---
+++ project/db_create.py
@@ -0,0 +1,24 @@
+"""create db for flasktaskr."""
+
+import sqlite3
+from _config import DATABASE_PATH
+
+with sqlite3.connect(DATABASE_PATH) as connection:
+
+    # get a cursor object used to execute SQL commands
+    c = connection.cursor()
+
+    # create the table
+    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
+        status INTEGER NOT NULL)""")
+
+    # insert dummy fata into the TABLE
+    c.execute(
+        'INSERT INTO tasks (name, due_date, priority, status)'
+        'VALUES("Finish this tutorial", "03/25/2015", 10, 1)'
+    )
+    c.execute(
+        'INSERT INTO tasks (name, due_date, priority, status)'
+        'VALUES("Finish Real Python Course 2", "03/25/2015", 10, 1)'
+    )
