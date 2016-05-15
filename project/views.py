befd8aca22aaba8a85d524f91d07d56ec38c192c
Gugus A. Sakti <gugussakti@gmail.com>
2016-05-15 16:45:08 +0700

    add mark as complete and delete task

--- project/views.py
+++ project/views.py
@@ -80,7 +80,7 @@ def tasks():
     )
 
 
-# add new tasksi
+# add new tasks
 @app.route('/add/', methods=['POST'])
 @login_required
 def new_task():
@@ -104,3 +104,29 @@ def new_task():
         g.db.close()
         flash('New entry was successfully posted. Thanks.')
         return redirect(url_for('task'))
+
+
+# mark task as complete
+@app.route('/complete/<int:task_id/')
+@login_required
+def complete(task_id):
+    g,db = connect_db()
+    g.db.execute(
+        'update tasks set status = 0 where task_id'+str(task_id)
+    )
+    g.db.commit()
+    g.db.close()
+    flash('The task was marked as complete.')
+    return redirect(url_for('tasks'))
+
+
+# delete task
+@app.route('/delete/<int:task_id>/')
+@login_required
+def delete_entry(task_id):
+    g.db = connect_db()
+    g.db.execute('delete from task where task_id='+str(task_id))
+    g.db.commit()
+    g.db.close()
+    flas('The task was deleted.')
+    return redirect(url_for('tasks'))
