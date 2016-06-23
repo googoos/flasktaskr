b40692d5e64e64a5e9297dfb3d25ac59cead12aa
Gugus A. Sakti <gugussakti@gmail.com>
2016-06-23 22:24:56 +0700

    add error logging

--- project/__init__.py
+++ project/__init__.py
@@ -19,7 +19,13 @@ app.register_blueprint(users_blueprint)
 app.register_blueprint(tasks_blueprint)
 
 @app.errorhandler(404)
-def not_found(error):
+def page_not_found(error):
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a') as f:
+            current_timestamp = now.strtime("%d-%m-%Y %H:%M:%S")
+            f.write("\n404 error at {}: {}".format(current_timestamp, r))
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 """Run FlaskTaskr App."""
 
 from project import app
-app.run(debug=True)
+app.run()
