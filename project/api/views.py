2aae8eb2c648612df588b12cb98e03db0e63d202
Gugus A. Sakti <gugussakti@gmail.com>
2016-06-24 22:55:40 +0700

    add api

--- project/api/views.py
+++ project/api/views.py
@@ -2,7 +2,8 @@
 
 
 from functools import wraps
-from flask import flash, redirect, jsonify, session, url_for, Blueprint
+from flask import flash, redirect, jsonify, session, url_for, Blueprint, \
+    make_response
 from project import db
 from project.models import Task
 
@@ -63,3 +64,25 @@ def api_tasks():
             }
         json_result.append(data)
     return jsonify(items=json_result)
+
+
+@api_blueprint.route('/api/v1/tasks/<int:task_id>')
+def task(task_id):
+    """API task."""
+    result = db.session.query(Task).filter_by(task_id=task_id).first()
+    if result:
+        result = {
+            'task_id': result.task_id,
+            'task.name': result.name,
+            'due_date': str(result.due_date),
+            'priority': result.priority,
+            'posted date': str(result.posted_date),
+            'status': result.status,
+            'user id': result.user_id
+        }
+        code = 200
+    else:
+        result = {"error": "Element does not exist"}
+        code = 404
+
+    return make_response(jsonify(result), code)
