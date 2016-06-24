651d9f6adf7a4ca1a20c8acb214e77e0ed1184c0
Gugus A. Sakti <gugussakti@gmail.com>
2016-06-24 17:26:48 +0700

    error log

--- project/__init__.py
+++ project/__init__.py
@@ -1,10 +1,11 @@
 # project/__init__.py
 
 
-from flask import Flask
+import datetime
+from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt
-from flask import render_template
+
 
 app = Flask(__name__)
 app.config.from_pyfile('_config.py')
@@ -14,9 +15,11 @@ db = SQLAlchemy(app)
 from project.users.views import users_blueprint
 from project.tasks.views import tasks_blueprint
 
+
 # register out blueprints
 app.register_blueprint(users_blueprint)
 app.register_blueprint(tasks_blueprint)
+
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -24,10 +27,19 @@ def page_not_found(error):
         now = datetime.datetime.now()
         r = request.url
         with open('error.log', 'a') as f:
-            current_timestamp = now.strtime("%d-%m-%Y %H:%M:%S")
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
             f.write("\n404 error at {}: {}".format(current_timestamp, r))
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_error(error):
+    db.session.rollback()
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write("\n500 error at {}: {}".format(current_timestamp, r))
     return render_template('500.html'), 500
+ 
